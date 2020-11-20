a2fdd3d8b5513ae6312ca17743083690e48238a6
dev-arekusandoru <66217747+dev-arekusandoru@users.noreply.github.com>
2020-11-19 20:07:29 -0500

    New Commit
    Added classes for User and Account, primitive design complete

--- src/test.java
+++ src/test.java
@@ -1,13 +1,17 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Enumeration;
 import javax.swing.*;
 import javax.swing.border.*;
+import javax.swing.plaf.FontUIResource;
 
 
 public class test implements ActionListener {
 
     boolean loggedIn = false;
+
+    static DefaultListModel<String> testList = new DefaultListModel<>();
 
     ////////---LOGIN FRAME VARS---////////
     JFrame loginFrame;
@@ -19,12 +23,18 @@ public class test implements ActionListener {
     ////////---MAIN FRAME VARS---////////
     JFrame mainFrame;
 
+    JLabel userInfoLabel;
+
     String username;
     String password;
 
     JButton testLogout;
 
+    JList accountViewer;
+
     public test() {
+        //set font to arial
+        setUIFont(new FontUIResource(new Font("Arial", 0, 15)));
 ////////////////////////////////////---Login Window---////////////////////////////////////
         loginFrame = new JFrame();
         loginFrame.setTitle("Login");
@@ -39,6 +49,7 @@ public class test implements ActionListener {
 
         //gets the main content panel
         JPanel mainLoginPanel = (JPanel) loginFrame.getContentPane();
+        mainLoginPanel.setBackground(Color.white);
 
         //sets the layout of the mainPanel
         mainLoginPanel.setLayout(new BoxLayout(mainLoginPanel, BoxLayout.Y_AXIS));
@@ -50,6 +61,7 @@ public class test implements ActionListener {
         //
         //login welcome panel
         JPanel loginWelcomePanel = new JPanel();
+        loginWelcomePanel.setBackground(Color.white);
         Border emptyWelcomeBorder = BorderFactory.createEmptyBorder(0, 0, 15, 0);
         loginWelcomePanel.setBorder(emptyWelcomeBorder);
         loginWelcomePanel.add(new JLabel("Please sign in to continue."));
@@ -57,6 +69,7 @@ public class test implements ActionListener {
         //
         //Assignment type selection
         JPanel loginInput = new JPanel();
+        loginInput.setBackground(Color.white);
         loginInput.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
 
@@ -64,12 +77,13 @@ public class test implements ActionListener {
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 0;
         c.gridy = 0;
-        loginInput.add(new JLabel("Username: "), c);
+        loginInput.add(new JLabel("<html><font color='red'>Username: </font></html>"), c);
 
         c.gridy = 1;
         loginInput.add(new JLabel("Password: "), c);
 
         loginUsername = new JTextField(15);
+        loginUsername.setSelectionColor(Color.black);
         c.gridwidth = 2;
         c.gridx = 1;
         c.gridy = 0;
@@ -80,8 +94,9 @@ public class test implements ActionListener {
         loginInput.add(loginPassword, c);
 
         //
-        //Add grade panel
+        //login button panel
         JPanel loginButtonPanel = new JPanel();
+        loginButtonPanel.setBackground(Color.white);
         login = new JButton("Login");
         login.addActionListener(this);
 
@@ -102,13 +117,14 @@ public class test implements ActionListener {
         //sets the location of the initial window
         mainFrame.setLocation(400, 150);
         mainFrame.setPreferredSize(new Dimension(400, 450));
+        mainFrame.setResizable(false);
 
         //exits the program when the window is closed
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //gets the main content panel
-        JPanel mainPanel = (JPanel)mainFrame.getContentPane();
-
+        JPanel mainPanel = (JPanel) mainFrame.getContentPane();
+        mainPanel.setBackground(Color.white);
         //sets the layout of the mainPanel
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
 
@@ -118,41 +134,89 @@ public class test implements ActionListener {
         //
         //user info panel
         JPanel userInfoPanel = new JPanel();
-        Border userBorder = new TitledBorder("USERNAME");
-        userInfoPanel.setBorder(userBorder);
+        userInfoPanel.setBackground(Color.white);
+        userInfoPanel.setLayout(new BoxLayout(userInfoPanel, BoxLayout.X_AXIS));
+        userInfoPanel.setBorder(new EmptyBorder(0, 0, 15, 0));
 
+
+        userInfoLabel = new JLabel("Welcome");
+
+        // icon scaling from user 'tirz'
+        // via https://stackoverflow.com/questions/16343098/resize-a-picture-to-fit-a-jlabel/16345968
         testLogout = new JButton("Logout");
-        testLogout.setBorderPainted(false);
-        testLogout.setFocusPainted(false);
-        //testLogout.setContentAreaFilled(false);
+        testLogout.setIcon(new ImageIcon(new ImageIcon("icons/logout_icon.png").getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH)));
+        testLogout.setHorizontalTextPosition(AbstractButton.LEADING);
+        testLogout.setPreferredSize(new Dimension(80, 100));
+        //testLogout.setBorder(new EtchedBorder());
         testLogout.addActionListener(this);
 
+        userInfoPanel.add(userInfoLabel);
+        userInfoPanel.add(Box.createHorizontalGlue());
         userInfoPanel.add(testLogout);
+
+        //
+        //user account list
+        JPanel userAccounts = new JPanel();
+        userAccounts.setBackground(Color.white);
+        Border accountBorder = new TitledBorder("Accounts");
+
+        accountViewer = new JList<String>((ListModel<String>) testList);
+        accountViewer.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        accountViewer.setPreferredSize(new Dimension(325, 200));
+
+        JScrollPane accountScroll = new JScrollPane(accountViewer,
+                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        accountScroll.getVerticalScrollBar().setPreferredSize(new Dimension(0, 0));
+        accountScroll.setBorder(accountBorder);
+        userAccounts.add(accountScroll);
 
         //add panels to main panel
         mainPanel.add(userInfoPanel);
+        mainPanel.add(userAccounts);
 
         mainFrame.pack();
-        mainFrame.setVisible(false);
+        mainFrame.setVisible(true);
+
 
     }
 
     public void actionPerformed(ActionEvent e) {
+
         Object control = e.getSource();
         if (control == login) {
             loginFrame.setVisible(false);
-
             username = loginUsername.getText();
+            userInfoLabel.setText("Welcome, " + username);
 
             mainFrame.setVisible(true);
         }
 
-        if(control == testLogout) {
+        if (control == testLogout) {
             System.out.println("success");
         }
     }
 
+    // font change method from Kumar Mitra
+    // via: https://stackoverflow.com/questions/12730230/set-the-same-font-for-all-component-java
+    public static void setUIFont(FontUIResource f) {
+        Enumeration keys = UIManager.getDefaults().keys();
+        while (keys.hasMoreElements()) {
+            Object key = keys.nextElement();
+            Object value = UIManager.get(key);
+            if (value instanceof FontUIResource) {
+                FontUIResource orig = (FontUIResource) value;
+                Font font = new Font(f.getFontName(), orig.getStyle(), f.getSize());
+                UIManager.put(key, new FontUIResource(font));
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
+        testList.add(0, "hello");
+        testList.addElement("goodbye");
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
